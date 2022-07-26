a36bc048ee8a2f66c3e1b2618891d36e2a20d272
DoubleYanLee <317549987@qq.com>
2022-07-27 02:53:03 +0800

    test_file_data_sink

--- src/test/java/ExampleDAG.java
+++ src/test/java/ExampleDAG.java
@@ -2,18 +2,27 @@ import client.ClientDAG;
 import client.Stream;
 import client.consumer.FileConsumerStream;
 import client.producer.FileProducerStream;
+import node.datasinks.FileDataSink;
+import node.datasources.FileDataSource;
 
 public class ExampleDAG {
 
     public static void main(String[] args){
 
         Stream source = new FileProducerStream("./test_DAG.txt");
+        //Stream consumer = new FileConsumerStream("./test_DAG_new.txt", source);
+        //ClientDAG compDag = ClientDAG.derive(consumer);
+        //System.out.println(compDag.toString());
 
-        Stream consumer = new FileConsumerStream("./test_DAG_new.txt", source);
+        FileDataSource fdsource = new FileDataSource(source.getID(), "./test_DAG.txt");
 
-        ClientDAG compDag = ClientDAG.derive(consumer);
+        /*
+        To do : 从FileDataSource中读取数据（op=NONE 先不操作）写入FileDataSink
+         */
 
-        System.out.println(compDag.toString());
+        FileDataSink fdsink = new FileDataSink(fdsource.getID(), "./test_DAG_new.txt");
+        fdsink.setUp();
+        fdsink.end();
 
     }
 
